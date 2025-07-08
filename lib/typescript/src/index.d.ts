5c301b9cdb202ec7b1792a8f5aa9f186b03f3196
Tim Vucina <tim.vucina@gmail.com>
2025-07-08 21:49:51 +0200

    fix(build): remove src from published files
    This prevents TypeScript tooling ambiguity that was breaking auto-import suggestions.

--- lib/typescript/src/index.d.ts
+++ lib/typescript/src/index.d.ts
@@ -1,2 +1,4 @@
 export { DebugProvider } from './components/debug/debug-provider';
+export { DbServerManager } from './db-server';
+export type { ServerStatus } from './db-server';
 //# sourceMappingURL=index.d.ts.map
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,3 @@
 export { DebugProvider } from './components/debug/debug-provider';
+export { DbServerManager } from './db-server';
+export type { ServerStatus } from './db-server';
